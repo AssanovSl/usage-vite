6a134d9a5f1efa8f8d29f7389a75633c71893e0c
Assanov <assanovvk.outlook.com>
2024-09-05 11:31:50 +0700

    work with html pages and image files

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -7,5 +10,23 @@ export default defineConfig({
   },
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
+    rollupOption: {
+      input: {
+        main: './src/index.html',
+      },
+    },
   },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+    injectHTML(),
+    ViteMinifyPlugin({}),
+  ],
 });
