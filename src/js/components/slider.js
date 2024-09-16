d0cca37a60fdeb852cd8e954074843d6b614febb
AssanovSl <AssanovSl@outlook.com>
2024-09-16 16:35:28 +0700

    added module nav in slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,12 +1,18 @@
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
 export const useInsightSlider = () => {
   new Swiper('.insight__slider', {
+    modules: [Navigation],
     slidesPerView: 'auto',
     spaceBetween: 32,
     loop: true,
     centeredSlides: true,
+    navigation: {
+      nextEl: '.insight__slider-next',
+      prevEl: '.insight__slider-prev',
+    },
     breakpoints: {
       992: {
         centeredSlides: false,
